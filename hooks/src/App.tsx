20578a90cdd8c5b999ee2e351dbd240737c39c88
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-07-19 18:47:21 -0300

    Estado local dos hooks

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 import useCounter from './hooks/useCounter';
+import PeopleList from './PeopleList';
 
 function App() {
   const number = useCounter()
   return (
     <div className="App">
       { number }
+      <PeopleList />
+      <PeopleList />
     </div>
   );
 }
