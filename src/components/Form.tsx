de1ad0dbb8f673b2eb0cdbed2b33fcb3bd07e6f6
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-07-19 18:13:24 -0300

    Boa maneira de lidar com formulário

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,64 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 export default function Form () {
-  const [name, setName] = useState('Arthur')
+  const [form, setForm] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    password: '',
+    passwordConfirm: ''
+  })
 
-  return <div>
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    console.log(form)
+  }
+
+  function handleInputChange (e: React.ChangeEvent<HTMLInputElement>) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  return <form onSubmit={handleSubmit}>
     <input
       placeholder="Nome"
       type="text"
-      value={name}
-      onChange={e => setName(e.target.value)}
+      name="name"
+      value={form.name}
+      onChange={handleInputChange}
     />
-    <div>
-      { name }
-    </div>
-  </div>
+    <input
+      placeholder="Sobrenome"
+      type="text"
+      name="lastName"
+      value={form.lastName}
+      onChange={handleInputChange}
+    />
+    <input
+      placeholder="Email"
+      type="email"
+      name="email"
+      value={form.email}
+      onChange={handleInputChange}
+    />
+    <input
+      placeholder="Senha"
+      name="password"
+      type="password"
+      value={form.password}
+      onChange={handleInputChange}
+    />
+    <input
+      placeholder="Confirme sua senha"
+      name="passwordConfirm"
+      type="passwordConfirm"
+      value={form.passwordConfirm}
+      onChange={handleInputChange}
+    />
+    <button type="submit">
+      enviar formulário
+    </button>
+  </form>
 }
