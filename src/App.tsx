2ebb29dc3e707ae1b01d6bff2c9af710ce812727
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-07-19 18:06:28 -0300

    Formularios

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import { useState } from 'react';
 import './App.css';
-import Card from './components/Card';
-import Post, { PostObject } from './components/Post';
+import Form from './components/Form';
 
 function App() {
-  const [post, setPost] = useState<PostObject | undefined>({
-    title: 'Título maneiro',
-    content: 'Lorem Ipsum dolor sit amet'
-  })
-
   return (
     <div className="App">
-    <header className="App-header">
-      {
-        !!post &&
-          <Card align="left" title={"Card"}>
-            <Post post={post} totalComments={12} />
-          </Card>
-      }
-      <button onClick={() => setPost(undefined)}>
-        Remover post
-      </button>
-    </header>
-  </div>
+      <header className="App-header">
+        <Form />
+      </header>
+    </div>
   );
 }
 
