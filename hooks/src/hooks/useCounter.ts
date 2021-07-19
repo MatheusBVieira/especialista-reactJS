c89bfc24e81f715f867c42f148743a14586aebba
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-07-19 19:34:05 -0300

    useCallback

--- hooks/src/hooks/useCounter.ts
+++ hooks/src/hooks/useCounter.ts
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export default function useCounter (initialNumber = 1) {
-  const [number, setNumber] = useState(initialNumber)
+export default function useCounter(initialNumber = 1) {
+  const [number, setNumber] = useState(initialNumber);
 
   useEffect(() => {
     setTimeout(() => {
-      setNumber(number + 1)
-    }, 5000)
-  }, [])
+      setNumber(number + 1);
+    }, 5000);
+  }, []);
 
-  return number
+  return number;
 }
