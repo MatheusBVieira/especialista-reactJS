c7ff33bd865dadbb44690ea6f1f5aa5b1ae27c96
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-07-20 13:59:51 -0300

    Cria variantes do botão

--- alganews-cms/src/stories/Button.stories.tsx
+++ alganews-cms/src/stories/Button.stories.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Button } from './Button';
+import Button, { ButtonProps } from "../components/Button/Button";
 
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 } as ComponentMeta<typeof Button>;
 
@@ -15,23 +15,18 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
+  variant: 'primary',
   label: 'Button',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
+export const Danger = Template.bind({});
+Danger.args = {
+  variant: 'danger',
   label: 'Button',
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
+export const Text = Template.bind({});
+Text.args = {
+  variant: 'text',
   label: 'Button',
 };
