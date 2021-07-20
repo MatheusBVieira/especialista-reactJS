b981545618218db05317810e2040102ed65838ac
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-07-20 14:28:41 -0300

    Componente Button com polished

--- alganews-cms/src/components/Button/Button.styles.ts
+++ alganews-cms/src/components/Button/Button.styles.ts
@@ -1,4 +1,11 @@
 import styled from "styled-components";
+import { transparentize } from 'polished';
+
+const COLORS = {
+  red: '#F84735',
+  primary: '#0099FF',
+  foreground: '#274060'
+}
 
 const THEME = {
   danger: {
@@ -6,21 +13,33 @@ const THEME = {
     color: '#FFFFFF',
     onHover: `
       box-shadow: 0 3px 6px rgba(0,0,0,.2);
-    `
+    `,
+    disabled: {
+      color: COLORS.red,
+      bg: transparentize(0.75, COLORS.red)
+    }
   },
   primary: {
     bg: '#0099FF',
     color: '#FFFFFF',
     onHover: `
       box-shadow: 0 3px 6px rgba(0,0,0,.2);
-    `
+    `,
+    disabled: {
+      color: '#fff',
+      bg: transparentize(0.44, COLORS.primary)
+    }
   },
   text: {
     bg: 'transparent',
     color: '#274060',
     onHover: `
       border-color: #274060;
-    `
+    `,
+    disabled: {
+      color: COLORS.foreground,
+      bg: transparentize(0.44, '#508AC9')
+    }
   }
 }
 
@@ -36,5 +55,13 @@ export const Wrapper = styled.button<{
   &:hover,
   &:focus {
     ${p => THEME[p.variant].onHover};
+  }
+
+  &:disabled {
+    background-color: ${p => THEME[p.variant].disabled.bg};
+    color: ${p => THEME[p.variant].disabled.color};
+
+    pointer-events: none;
+    border-color: transparent;
   }
 `
--- alganews-cms/src/stories/Button.stories.tsx
+++ alganews-cms/src/stories/Button.stories.tsx
@@ -16,24 +16,24 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   variant: 'primary',
-  label: 'Button',
+  label: 'resetar senha',
 };
 
 export const Danger = Template.bind({});
 Danger.args = {
   variant: 'danger',
-  label: 'Button',
+  label: 'resetar senha',
 };
 
 export const Text = Template.bind({});
 Text.args = {
   variant: 'text',
-  label: 'Button',
+  label: 'resetar senha',
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   variant: 'primary',
   disabled: true,
-  label: 'Button',
+  label: 'resetar senha',
 };
