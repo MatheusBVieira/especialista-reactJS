e6161ab523af03ab25cefa4872c4c6a7dccfd0f4
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-07-28 19:15:45 -0300

    Extrai props do Chart

--- alganews-cms/src/components/Chart/Chart.tsx
+++ alganews-cms/src/components/Chart/Chart.tsx
@@ -1,31 +1,8 @@
 import { transparentize } from 'polished';
 import { Line } from 'react-chartjs-2';
 import styled from 'styled-components';
+import NoData from '../NoData/NoData';
 import Heading from '../Typography/Heading';
-
-const data = {
-  labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
-  datasets: [
-    {
-      label: 'Receitas',
-      data: [500, 400, 600, 100, 800, 20],
-      fill: true,
-      backgroundColor: '#0099FF',
-      borderColor: '#0099FF',
-      borderWidth: 0.5,
-      yAxisID: 'cashflow',
-    },
-    {
-      label: 'Despesas',
-      data: [100, 200, 250, 500, 1000, 600],
-      fill: true,
-      backgroundColor: '#274060',
-      borderColor: '#274060',
-      borderWidth: 0.5,
-      yAxisID: 'cashflow',
-    },
-  ],
-};
 
 const options: Chart.ChartOptions = {
   maintainAspectRatio: true,
@@ -62,22 +39,29 @@ const options: Chart.ChartOptions = {
   },
 };
 
-export interface ChartProps {}
+export interface ChartProps {
+  data: Chart.ChartData
+  title: string
+}
 
-export default function Chart () {
+export default function Chart ({ data, title }: ChartProps) {
   return <ChartWrapper style={{ width: 700 }}>
     <div style={{ marginBottom: 16 }}>
       <Heading level={3}>
-        {'Média de performance nos últimos 12 meses'}
+        {title}
       </Heading>
     </div>
-    <Line
-      type="line"
-      height={139}
-      width={600}
-      data={data}
-      options={options}
-    />
+    {
+      data
+        ? <Line
+            type="line"
+            height={139}
+            width={600}
+            data={data}
+            options={options}
+          />
+        : <NoData height={139} />
+    }
   </ChartWrapper>
 }
 
