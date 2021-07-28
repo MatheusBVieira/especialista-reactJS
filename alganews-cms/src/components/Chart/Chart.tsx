fed37b376376154a268f4e0ec5f7705409184932
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-07-28 19:02:53 -0300

    Ajusta eixos do Chart

--- alganews-cms/src/components/Chart/Chart.tsx
+++ alganews-cms/src/components/Chart/Chart.tsx
@@ -8,16 +8,18 @@ const data = {
       data: [500, 400, 600, 100, 800, 20],
       fill: true,
       backgroundColor: '#0099FF',
-      borderColor: 'transparent',
-      yAxisID: 'y-axis-1',
+      borderColor: '#0099FF',
+      borderWidth: 0.5,
+      yAxisID: 'cashflow',
     },
     {
       label: 'Despesas',
-      data: [1, 2, 1, 1, 2, 2],
+      data: [100, 200, 250, 500, 1000, 600],
       fill: true,
       backgroundColor: '#274060',
-      borderColor: 'transparent',
-      yAxisID: 'y-axis-2',
+      borderColor: '#274060',
+      borderWidth: 0.5,
+      yAxisID: 'cashflow',
     },
   ],
 };
@@ -43,17 +45,8 @@ const options: Chart.ChartOptions = {
         type: 'linear',
         display: true,
         position: 'left',
-        id: 'y-axis-1',
-      },
-      {
-        type: 'linear',
-        display: true,
-        position: 'right',
-        id: 'y-axis-2',
-        gridLines: {
-          display: false
-        },
-      },
+        id: 'cashflow',
+      }
     ],
   },
 };
