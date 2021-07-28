e245396a3148fc7e96de489a1863a6a9cf41cce8
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-07-28 18:54:47 -0300

    Configura linhas retas no Chart

--- alganews-cms/src/components/Chart/Chart.tsx
+++ alganews-cms/src/components/Chart/Chart.tsx
@@ -24,6 +24,11 @@ const data = {
 
 const options: Chart.ChartOptions = {
   maintainAspectRatio: false,
+  elements: {
+    line: {
+      tension: 0
+    }
+  },
   legend: {
     display: true,
     position: 'bottom',
