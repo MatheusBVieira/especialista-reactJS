e555faa62cf7fbf9da78b4215d58560eed9f6334
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-08-18 22:55:08 -0300

    Estiliza menu lateral

--- alganews-cms/src/app/components/NavBar.tsx
+++ alganews-cms/src/app/components/NavBar.tsx
@@ -1,10 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 export default function NavBar () {
-  return <nav>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/contato">Contato</Link></li>
-    </ul>
-  </nav>
+  return <>
+    <List>
+      <Item><NavLink exact to="/">Home</NavLink></Item>
+      <Item><NavLink exact to="/contato">Contato</NavLink></Item>
+    </List>
+  </>
 }
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  list-style: none;
+`
+
+const Item = styled.li`
+  text-transform: lowercase;
+  font-size: 18px;
+
+  a {
+    text-decoration: none;
+    color: #274060;
+
+    &.active {
+      color: #09f;
+    }
+  }
+`
+
