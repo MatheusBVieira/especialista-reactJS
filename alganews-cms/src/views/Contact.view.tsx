4a978592102bc1358560e4d3418553e384af8779
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-08-18 22:17:34 -0300

    Altera titulo da pagina conforme a rota

--- alganews-cms/src/views/Contact.view.tsx
+++ alganews-cms/src/views/Contact.view.tsx
@@ -1,4 +1,7 @@
+import usePageTitle from "../hooks/usePageTitle"
+
 export default function Contact () {
+  usePageTitle('Contato')
   return <div>
     <h1>Contato</h1>
   </div>
--- alganews-cms/src/views/Home.view.tsx
+++ alganews-cms/src/views/Home.view.tsx
@@ -1,4 +1,7 @@
+import usePageTitle from "../hooks/usePageTitle"
+
 export default function Home () {
+  usePageTitle('Home')
   return <div>
     <h1>Home</h1>
   </div>
