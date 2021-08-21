4b969a03be9d8a941019ce6ec907a6441bb54327
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2021-08-21 13:35:47 -0300

    Envia requisição com Fetch

--- alganews-cms/src/index.tsx
+++ alganews-cms/src/index.tsx
@@ -26,6 +26,33 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+async function getDataFromApi() {
+  try {
+    const response = await fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: 'Olá, mundo',
+        body: 'Lorem Ipsum dolor sit amet'
+      })
+    })
+    console.log('Response:', response)
+    if (response.status >= 400) {
+      throw new Error(await response.json())
+    }
+    const posts = await response.json()
+    console.log('Sucesso')
+    console.log(posts)
+  } catch (error) {
+    console.log('Houve erro')
+    console.log(error.message)
+  }
+}
+
+getDataFromApi()
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
