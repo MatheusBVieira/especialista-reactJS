e4b0d4495fa66802e55519fd60867e1c34d59870
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2022-02-16 23:15:57 -0300

    Cria FileService

--- alganews-cms/src/sdk/@types/index.ts
+++ alganews-cms/src/sdk/@types/index.ts
@@ -3,6 +3,7 @@ export type { CashFlow } from './CashFlow'
 export type { Metric } from './Metric'
 export type { User } from './User'
 export type { Payment } from './Payment'
+export type { File } from './File'
 
 // AlgaNews
 export type { AlgaNews } from './AlgaNews'
